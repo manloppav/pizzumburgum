c9cb261608c7835a06a396b1508a762376bb3971
Usuario <mfernandez1934@gmail.com>
2025-10-14 19:30:42 -0300

    a

---
+++ src/main/java/com/example/pizzumburgum/components/Usuario.java
@@ -0,0 +1,39 @@
+package com.example.pizzumburgum.components;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUsuario;
+
+    @Column(nullable = false, length = 50)
+    private String nombre;
+
+    @Column(nullable = false, length = 50)
+    private String apellido;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 100)
+    private String hashPassword;
+
+    @Column(nullable = false, unique = true, length = 20)
+    private String documento;
+
+    @Column(name = "fecha_nac", nullable = false)
+    private java.time.LocalDate fechaNacimiento;
+
+    @Column(length = 15)
+    private String celular;
+}
+
