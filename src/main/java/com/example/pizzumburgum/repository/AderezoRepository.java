6f9ec50d6a50c3991e38c7bd6781e8411c2561ad
jromero04 <jromero4@correo.um.edu.uy>
2025-10-16 16:21:22 -0300

    repositorios, direccion y Tarjeta

--- src/main/java/com/example/pizzumburgum/repository/AderezoRepository.java
+++ src/main/java/com/example/pizzumburgum/repository/AderezoRepository.java
@@ -1,6 +1,11 @@
 package com.example.pizzumburgum.repository;
 
+import com.example.pizzumburgum.component.Aderezo;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AderezoRepository extends JpaRepository<Aderezo, Long> {
