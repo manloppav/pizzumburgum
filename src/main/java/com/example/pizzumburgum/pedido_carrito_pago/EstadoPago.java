d88b72af22963a0879a6a26c79e033b60dace93a
Usuario <mfernandez1934@gmail.com>
2025-10-16 16:03:00 -0300

    SE BORRO TODO

--- src/main/java/com/example/pizzumburgum/pedido_carrito_pago/EstadoPago.java
+++
@@ -1,8 +0,0 @@
-package com.example.pizzumburgum.pedido_carrito_pago;
-
-public enum EstadoPago {
-    PENDIENTE,
-    APROBADO,
-    RECHAZADO,
-    ANULADO
-}
--- src/main/java/com/example/pizzumburgum/pedido_carrito_pago/EstadoPedido.java
+++
@@ -1,9 +0,0 @@
-package com.example.pizzumburgum.pedido_carrito_pago;
-
-public enum EstadoPedido {
-    EN_COLA,
-    EN_PREPARACION,
-    EN_CAMINO,
-    ENTREGADO,
-    CANCELADO
-}
